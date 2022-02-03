2059ec0f7007f6d40f5b690280e9106754c75625
Binsanity95 <84565641+Leeselbin@users.noreply.github.com>
2022-02-03 14:36:01 +0900

    1

---
+++ sercurity1/src/main/java/com/cos/sercurity1/Sercurity1Application.java
@@ -0,0 +1,13 @@
+package com.cos.sercurity1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sercurity1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sercurity1Application.class, args);
+	}
+
+}
---
+++ sercurity1/src/test/java/com/cos/sercurity1/Sercurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.sercurity1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sercurity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
